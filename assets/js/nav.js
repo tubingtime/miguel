c39f8db003cece0f2cf87c0347e2262fde7a360b
tubingtime <tdelaveaga@dons.usfca.edu>
2022-10-02 17:08:22 -0700

    added feature: nav to highlight what page were on

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -5,4 +5,9 @@ fetch('../../nav.html')
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
+    let fileName = (location.pathname.replace("/",""));
+    if (fileName == "") {
+        fileName = "index.html"
+    }
+    document.getElementById(fileName).classList.add("active")
 })
